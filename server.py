a80bc800eabd42f7e31288f694ce882cdabd3802
Diego <lima.j.diego@gmail.com>
2021-06-10 12:49:33 -0300

    fazendo ligação cliente/servidor

--- server.py
+++ server.py
@@ -2,3 +2,20 @@ import socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 print(' Socket criado com sucesso')
+
+host = 'localhost'
+port = 5432
+
+# Ligação cliente servidor com bind
+
+s.bind((host, port))
+msg = '\nServidor: Olá Cliente'
+
+while 1:
+    dados, end = s.recvfrom(4096)
+
+    if dados:
+        print('Servidor enviando mensagem...')
+        s.sendto(dados + (msg.encode()), end)
+
+
