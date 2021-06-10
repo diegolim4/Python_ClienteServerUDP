b8fdc1fc5a0eb648718e34a875c340751013e54a
Diego <lima.j.diego@gmail.com>
2021-06-10 12:10:58 -0300

    Try para enviar e receber a mensagem

--- client.py
+++ client.py
@@ -7,3 +7,15 @@ print('Cliente Socket criado com sucesso!')
 host = 'localhost:'
 port = 5433
 msg = 'Olá Servidor!'
+
+# enviar e receber a mensagem
+try:
+    print(f'Cliente {msg}')
+    s.sendto(msg.encode(), (host, 5432))
+
+    dados, servidor = s.recvfrom(4096)  # 4096 Bytes
+    dados = dados.decode()
+    print(f'Cliente: {dados}')
+finally:
+    print('Cliente: Fechando a Conexão')
+    s.close()
